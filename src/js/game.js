8d56dda633827bb1588fb7ce38481107c2f8f70a
Ollie Baker <oliverjohnbaker@gmail.com>
2015-12-09 17:47:45 +0000

    splitting creation of initial grid into chunks to animate + stop initial slowdown

--- src/js/game.js
+++ src/js/game.js
@@ -6,7 +6,7 @@
     Game.prototype = {
         create: function() {
 
-            this.isRunning = true;
+            this.isRunning = false;
 
             this.CELL_SIZE = 4;
             this.GRID_SIZE = Math.floor(this.game.height / this.CELL_SIZE);
@@ -33,26 +33,53 @@
             this.neighbours.push(new Point(1, 1));
 
             //calc offset on x axis so our grid is centered
-            var offsetX = (this.game.width - (this.GRID_SIZE * this.CELL_SIZE)) / 2;
+            this.offsetX = (this.game.width - (this.GRID_SIZE * this.CELL_SIZE)) / 2;
 
-            //populate our grid with cells
-            for (var x = 0; x < this.GRID_SIZE; x++) {
-                for (var y = 0; y < this.GRID_SIZE; y++) {
-                    var cell = new Cell(this.game, x, y, this.CELL_SIZE);
-                    cell.x += offsetX;
+            //keep track of which cells have been created since we don't use a loop
+            this.xPop = 0;
+            this.yPop = 0;
+
+            //create a timer so we can track when all cells are complete
+            var chunkTimer = this.game.time.create(false);
+            chunkTimer.onComplete.addOnce(function() {
+                //start the sim and allow the user control;
+                this.addEventListeners();
+                this.isRunning = true;
+            }, this);
+
+            //run the timer ~every frame, create one row at a time;
+            chunkTimer.repeat(1 / 60, this.GRID_SIZE, this.populateGrid, this);
+            chunkTimer.start();
+            
+        },
+
+        populateGrid: function() {
+            for (var i = 0; i <= this.GRID_SIZE; i++) {
+
+                //manually incrementing our array index since were not using a nested loop.
+                if (this.xPop < this.GRID_SIZE) {
+
+                    var cell = new Cell(this.game, this.xPop, this.yPop, this.CELL_SIZE);
+                    cell.x += this.offsetX;
                     //use a flat 2D array structure to minimise array access
-                    this.grid[x * this.GRID_SIZE + y] = cell;
+                    this.grid[this.xPop * this.GRID_SIZE + this.yPop] = cell;
+                    this.xPop++;
+
+                } else {
+                    this.xPop = 0;
+                    this.yPop++;
                 }
-            }
+            };
+        },
+
+        addEventListeners: function () {
+            this.input.onDown.add(this.togglePause, this);
 
             var restartBtn = document.getElementById('restartBtn');
             restartBtn.onclick = this.restart.bind(this);
 
             var pauseBtn = document.getElementById('pauseBtn');
             pauseBtn.onclick = this.togglePause.bind(this);
-
-            //start the simulation after the user's clicked, also allows pausing the sim
-            this.input.onDown.add(this.togglePause, this);
         },
 
         update: function() {
