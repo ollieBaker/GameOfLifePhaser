dd94e114ab0cd1cd3ce2447e44dd559a3f5e1263
Ollie Baker <oliverjohnbaker@gmail.com>
2015-12-09 17:08:33 +0000

    Added fade to menu screen

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,13 +5,13 @@
 
     Menu.prototype = {
         create: function() {
-            var text = this.add.text(this.game.width * 0.5, this.game.height * 0.5,
+            this.titleTxt = this.add.text(this.game.width * 0.5, this.game.height * 0.5,
                 'This is an implementation of\nConway\'s Game of Life \n using JavaScript and Phaser', {
                     font: '32px Arial',
                     fill: '#ffffff',
                     align: 'center'
                 });
-            text.anchor.set(0.5);
+            this.titleTxt.anchor.set(0.5);
             this.input.onDown.add(this.onDown, this);
         },
 
@@ -20,7 +20,12 @@
         },
 
         onDown: function() {
-            this.game.state.start('game');
+            this.game.add.tween(this.titleTxt).to({
+                alpha: 0
+            }, 700, Phaser.Easing.Linear.None, true).onComplete.addOnce(function() {
+                this.game.state.start('game')
+            }, this);
+
         }
     };
 
